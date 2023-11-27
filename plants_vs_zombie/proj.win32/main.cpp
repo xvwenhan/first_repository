393b0a222f21380ca5b5b9f35c7b7f63582f8780
xvwenhan <xvwenhan@outlook.com>
2023-11-27 19:24:07 +0800

    试验一下

--- plants_vs_zombie/proj.win32/main.cpp
+++ plants_vs_zombie/proj.win32/main.cpp
@@ -21,7 +21,7 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
  ****************************************************************************/
-
+///////////////////////////////////////////////////////////////////////////// 
 #include "main.h"
 #include "AppDelegate.h"
 #include "cocos2d.h"
